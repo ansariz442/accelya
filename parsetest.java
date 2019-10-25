dd4a765d2537e5dd7fac491db9e597695c13b8fe
ansariz442 <ansariz442@gmail.com>
2019-10-25 15:37:53 +0530

    Add files via upload
    Java Program for use of .valueOf() and .parseDouble().

---
+++ parsetest.java
@@ -0,0 +1,14 @@
+
+public class parsetest {
+
+	public static void main(String[] args) {
+		String s= "null";
+		Double d=Double.valueOf(s);
+		double d1 = Double.parseDouble(s);
+		//if(d instanceof Double)
+		System.out.println(d);
+		System.out.println(d1);
+
+	}
+
+}
